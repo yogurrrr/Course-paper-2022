0ef923365726f36c47cbc342dc42e020660f0e05
Andrei Semennikov <aasemennikov@edu.hse.ru>
2022-06-04 19:01:16 +0300

    Fixed the need for a reset button

--- src/main/java/com/TSP/Controller.java
+++ src/main/java/com/TSP/Controller.java
@@ -1,11 +1,14 @@
 package com.TSP;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
-import java.util.Scanner;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
@@ -14,6 +17,7 @@ import javafx.scene.shape.Circle;
 import javafx.geometry.Point2D;
 import javafx.scene.control.Button;
 import javafx.scene.shape.Line;
+import javafx.stage.Stage;
 
 public class Controller {
 
@@ -29,11 +33,30 @@ public class Controller {
     @FXML
     private Pane canvasPane;
 
+    private Point2D canvasPanePointCoords;
+
     @FXML
     private Button resetButton;
 
     @FXML
     private Button startButton;
+
+    @FXML
+    private Button distanceInputButton;
+
+    @FXML
+    private TextField distanceInputField;
+
+    @FXML
+    private AnchorPane distanceInputWindow;
+
+    private void showDistanceInputWindow(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("dialog.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 400, 250);
+        stage.setTitle("Distance input");
+        stage.setScene(scene);
+        stage.show();
+    }
 
     private ArrayList<Point2D> pointsArray;
 
@@ -49,6 +72,8 @@ public class Controller {
         }
         return i + 1;
     }
+
+    private int repPointClicksCounter;
 
     @FXML
     void onMouseClicked(MouseEvent event) {
@@ -78,24 +103,24 @@ public class Controller {
                 System.out.println("Added new city: (" + canvasPanePointCoords.getX() + " ; " + canvasPanePointCoords.getY() + ")");
                 newCitiesCount++;
             } else if (flag == 0) { //если в месте клика уже есть точка
-                if (prevPointClicked != canvasPanePointCoords && pointsArray.size() > 1) { //если пред. клик был не в эту же точку
-                    for (int i = 0; i < pointsArray.size(); ++i) {
-                        if (pointsArray.get(i).distance(canvasPanePointCoords) < 20 || pointsArray.get(i).distance(prevPointClicked) < 20)
-                            ++counter2;
+                if (pointsArray.size() > 1) { //если пред. клик был не в эту же точку и имеется больше 1 точки
+                    repPointClicksCounter++;
+                    if (repPointClicksCounter == 2) {
+                        for (int i = 0; i < pointsArray.size(); ++i) {
+                            if (pointsArray.get(i).distance(canvasPanePointCoords) < 20 || pointsArray.get(i).distance(prevPointClicked) < 20)
+                                ++counter2;
+                        }
+                        repPointClicksCounter = 0;
                     }
-                    if (counter2 == 2) { //если пред. клик и текущий клик - города
-                        //прописать защиту от соединения с последним кликнутым городом
+                    if (counter2 == 2 && prevPointClicked.distance(canvasPanePointCoords) > 20) { //если пред. клик и текущий клик - города
                         System.out.println("Введите расстояние между городами " + getCity(prevPointClicked) + " и " + getCity(canvasPanePointCoords));
-                        //сделать появление диалогового окна вместо ввода в консоли
-                        Scanner in = new Scanner(System.in);
-                        int temp_input = in.nextInt();
-                        CitiesCollection.addNewRoad(getCity(prevPointClicked), getCity(canvasPanePointCoords), temp_input);
-                        Line line = new Line();
-                        line.setStartX(prevPointClicked.getX());
-                        line.setStartY(prevPointClicked.getY());
-                        line.setEndX(canvasPanePointCoords.getX());
-                        line.setEndY(canvasPanePointCoords.getY());
-                        canvasPane.getChildren().add(line);
+                        Stage stage = new Stage();
+                        try {
+                            showDistanceInputWindow(stage);
+                        }
+                        catch (IOException e) {
+                            System.out.println("Wrong input!");
+                        }
                     }
                 }
             }
@@ -109,16 +134,32 @@ public class Controller {
     }
 
     @FXML
-    void onResetButtonClicked(MouseEvent event) {
-        prevPointClicked.subtract(prevPointClicked);
+    void onInputButtonClicked(MouseEvent event) {
+        String input = distanceInputField.getText();
+        if (!(input.isEmpty())) {
+            if (Integer.parseInt(input) > 0) {
+                CitiesCollection.addNewRoad(getCity(prevPointClicked), getCity(canvasPanePointCoords), Integer.parseInt(input));
+                Line line = new Line();
+                line.setStartX(prevPointClicked.getX());
+                line.setStartY(prevPointClicked.getY());
+                line.setEndX(canvasPanePointCoords.getX());
+                line.setEndY(canvasPanePointCoords.getY());
+                canvasPane.getChildren().add(line);
+            }
+        }
     }
 
     @FXML
     void initialize() {
+        //Pane tempPane = new Pane();
+        //canvasPane = tempPane;
         CitiesCollection.createCitiesCollection();
         pointsArray = new ArrayList<>();
+        Point2D tempPoint = new Point2D(0,0);
+        canvasPanePointCoords = tempPoint;
         newCitiesCount = 1;
         prevPointClicked = new Point2D(0,0);
+        repPointClicksCounter = 0;
     }
 
 }
