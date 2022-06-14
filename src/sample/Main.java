056cf5052bc91d0b01ea3a4b3edb62071e7c9b67
Akenten <ars.alex19@yandex.ru>
2022-06-14 12:27:18 +0300

    added icon, minor changes

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,18 +4,20 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.shape.Line;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Расскраска графа");
+        primaryStage.setTitle("Раскраска графа");
 
 
         primaryStage.setScene(new Scene(root, 700, 400));
+        primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("icon.png")));
         primaryStage.show();
 
     }
